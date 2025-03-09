cfae5a024ca5031899e610c14597caaa958e1b7d
orbitshift-developer <os-dev@orbitshift.ai>
2025-03-09 22:35:05 +0530

    initail setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import "@/styles/global.css";
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl">Hello World</Text>
+    </View>
+  );
+}
